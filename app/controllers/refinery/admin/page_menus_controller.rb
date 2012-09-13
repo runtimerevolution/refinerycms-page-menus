749992141a8c0c0a395443ba6aa28a3401c2041f
Alexandre Quiterio <quiterio7@gmail.com>
2012-09-13 12:34:13 +0200

    Update app/controllers/refinery/admin/page_menus_controller.rb

--- app/controllers/refinery/admin/page_menus_controller.rb
+++ app/controllers/refinery/admin/page_menus_controller.rb
@@ -5,7 +5,6 @@ module Refinery
       crudify :'refinery/page_menu', :xhr_paging => true, :sortable => false, :redirect_to_url => "refinery.admin_page_menu_page_positions_path(@page_menu)"
 
       def edit_main_menu
-        @page_menu = Refinery::PageMenu.new
       end
       
       def update_main_menu 
